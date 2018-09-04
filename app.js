482cbb279578f068ff9eb1abdadc7e755f96fb52
ryang <ryang@nfsmith.com>
2018-09-04 16:57:36 +0800

    develop

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //app.js
+
+const WXBizDataCrypt = require('utils/WXBizDataCrypt.js')
 App({
   onLaunch: function () {
     // 展示本地存储能力
@@ -6,10 +8,32 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
+    var appId = 'wx8174f411e9ee8e2d';
+    var secret = 'd578eb87a504ec37d99cd627b9853aff';
+
+    var sessionKey = '';
+    
+
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+
+        var code = res.code; //返回code
+        console.log(code);
+        
+        wx.request({
+          url: 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appId + '&secret=' + secret + '&js_code=' + code + '&grant_type=authorization_code',
+          data: {},
+          header: {
+            'content-type': 'json'
+          },
+          success: function (res) {
+            var openid = res.data.openid; //返回openid
+            sessionKey = res.data.session_key;
+          }
+        })
+        console.info('sk0' + sessionKey)
       }
     })
     // 获取用户信息
@@ -20,6 +44,17 @@ App({
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
+              var encryptedData = res.encryptedData;
+              var iv = res.iv;
+              var pc = new WXBizDataCrypt(appId, sessionKey);
+
+              console.info('sk1'+sessionKey);
+              var data = pc.decryptData(encryptedData, iv);
+
+              // console.info(data);
+
+
+
               this.globalData.userInfo = res.userInfo
               console.info(res)
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
--- pages/index/index.js
+++ pages/index/index.js
@@ -42,13 +42,5 @@ Page({
         }
       })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
   }
 })
