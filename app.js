b8602466dce0ee23a7b4c451f0cab28265f90ae4
Ray <ryang@nfsmith.com>
2018-09-05 00:35:17 +0800

    init

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 //app.js
 
-const WXBizDataCrypt = require('utils/WXBizDataCrypt.js')
 App({
-  onLaunch: function () {
+  onLaunch: function() {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    var appId = 'wx8174f411e9ee8e2d';
-    var secret = 'd578eb87a504ec37d99cd627b9853aff';
-
-    var sessionKey = '';
-    
+    wx.setStorageSync('logs', logs);
 
     // 登录
     wx.login({
@@ -20,20 +13,17 @@ App({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
 
         var code = res.code; //返回code
-        console.log(code);
-        
+
         wx.request({
-          url: 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appId + '&secret=' + secret + '&js_code=' + code + '&grant_type=authorization_code',
+          url: 'https://api.weixin.qq.com/sns/jscode2session?appid=' + this.globalData.app + '&secret=' + this.globalData.secret + '&js_code=' + code + '&grant_type=authorization_code',
           data: {},
           header: {
             'content-type': 'json'
           },
-          success: function (res) {
-            var openid = res.data.openid; //返回openid
-            sessionKey = res.data.session_key;
+          success: res => {
+            this.globalData.openId = res.data.openid;
           }
         })
-        console.info('sk0' + sessionKey)
       }
     })
     // 获取用户信息
@@ -44,19 +34,8 @@ App({
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              var encryptedData = res.encryptedData;
-              var iv = res.iv;
-              var pc = new WXBizDataCrypt(appId, sessionKey);
-
-              console.info('sk1'+sessionKey);
-              var data = pc.decryptData(encryptedData, iv);
-
-              // console.info(data);
-
-
-
+              console.info(res.userInfo);
               this.globalData.userInfo = res.userInfo
-              console.info(res)
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
@@ -69,6 +48,9 @@ App({
     })
   },
   globalData: {
+    appId: 'wx8174f411e9ee8e2d',
+    secret: 'd578eb87a504ec37d99cd627b9853aff',
+    openId: null,
     userInfo: null
   }
 })
